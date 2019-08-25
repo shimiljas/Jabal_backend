edf4101ca063b23dc5fc7d7869037df6e382cc43
kj@1echo.us <kj@1echo.us>
2019-08-25 17:56:06 +0530

    message

--- utils/fetchLimit.js
+++ utils/fetchLimit.js
@@ -4,7 +4,6 @@ exports.fetchLimit = () => {
   return new Promise((resolve, reject) => {
     Limit.findOne({}, {}, { sort: { created_at: -1 } })
       .then(res => {
-        console.log(res, 'xxxxxx=========');
         if (res == null) {
           resolve({ limit: 1000, discount: 30 });
         } else {
