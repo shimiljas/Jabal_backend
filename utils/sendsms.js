795178123461e0bf66bfd9787dd822f8237297bf
shimil <kj@1ech.us>
2020-03-09 12:28:05 +0530

    live credntial

--- utils/sendsms.js
+++ utils/sendsms.js
@@ -1,11 +1,11 @@
 const config = {
-  accountSid: 'AC28b4b082cf5258c064daac8276d44783',
-  authToken: '94c05d8f5f6d5d4e6a47987be5953a83',
-  sender: '+13098071877',
+  accountSid: 'ACfb04543343e50fba31bcf5efe8ea00bc',
+  authToken: 'cb10bc70b15fe71fb2e275996005d048',
+  sender: '+12073864114',
   AUTHY_API_KEY: 'boygRptasuc2OwU19UnsH5Uy6hUEvNhv'
 }
 const client = require('twilio')(config.accountSid, config.authToken)
-
+const authy = require('authy')(config.AUTHY_API_KEY)
 const http = require('http');
 const urlencode = require('urlencode');
 
@@ -46,47 +46,47 @@ exports.sendbytextlocal = (total, discount, mobile) => {
         : `Thank you for purchasing from Zadi Boutique.You have completed a purchasing of ${total}.00 rupees.`;
 
         
-      client.messages
-        .create({ body: message, from: '+13098071877', to: `+91${mobile}` })
-        .then(message => {
-          console.log('success', message);
-          resolve(message);
-        })
-        .catch(err => {
-          console.log('twilio', err);
-          reject(err);
-        });
-    // var msg = urlencode(message);
+      // client.messages
+      //   .create({ body: message, from:config.sender, to: `+91${mobile}` })
+      //   .then(message => {
+      //     console.log('success', message);
+      //     resolve(message);
+      //   })
+      //   .catch(err => {
+      //     console.log('twilio', err);
+      //     reject(err);
+      //   });
+    var msg = urlencode(message);
 
-    // var toNumber = `+91${mobile}`;
-    // var username = 'Jabalminimarket@gmail.com';
-    // var hash =
-    //   '08eb9374e3fdca007c984045095f63ef6eb1994f5d5d58870fb1f215145070d5'; // The hash key could be found under Help->All Documentation->Your hash key. Alternatively you can use your Textlocal password in plain text.
-    // var sender = 'txtlcl';
-    // var data =
-    //   'username=' +
-    //   username +
-    //   '&hash=' +
-    //   hash +
-    //   '&sender=' +
-    //   sender +
-    //   '&numbers=' +
-    //   toNumber +
-    //   '&message=' +
-    //   msg;
-    // var options = {
-    //   host: 'api.textlocal.in',
-    //   path: '/send?' + data,
-    // };
-    // callback = function(response) {
-    //   var str = ''; 
-    //   response.on('data', function(chunk) {
-    //     str += chunk;
-    //   }); 
-    //   response.on('end', function() {
-    //     resolve(message);
-    //   });
-    // }; 
-    // http.request(options, callback).end(); 
+    var toNumber = `+91${mobile}`;
+    var username = 'Jabalminimarket@gmail.com';
+    var hash =
+      '08eb9374e3fdca007c984045095f63ef6eb1994f5d5d58870fb1f215145070d5'; // The hash key could be found under Help->All Documentation->Your hash key. Alternatively you can use your Textlocal password in plain text.
+    var sender = 'txtlcl';
+    var data =
+      'username=' +
+      username +
+      '&hash=' +
+      hash +
+      '&sender=' +
+      sender +
+      '&numbers=' +
+      toNumber +
+      '&message=' +
+      msg;
+    var options = {
+      host: 'api.textlocal.in',
+      path: '/send?' + data,
+    };
+    callback = function(response) {
+      var str = ''; 
+      response.on('data', function(chunk) {
+        str += chunk;
+      }); 
+      response.on('end', function() {
+        resolve(message);
+      });
+    }; 
+    http.request(options, callback).end(); 
   });
 };
